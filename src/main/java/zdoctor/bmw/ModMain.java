2d309cbf52e489b4f3cd2efbea5d6b9f75fa80a2
Reuben J Renquist <seedofdiscovery@gmail.com>
2016-06-17 22:29:56 -0700

    gitignore test and version change

--- src/main/java/zdoctor/bmw/ModMain.java
+++ src/main/java/zdoctor/bmw/ModMain.java
@@ -10,7 +10,7 @@ import zdoctor.bmw.common.CommonProxy;
 public class ModMain {
 	public static final String MODID = "ZDoctorBMW";
 	public static final String NAME = "Blood Magic Wiki";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "0.1";
 	@net.minecraftforge.fml.common.SidedProxy(clientSide = "zdoctor.bmw.client.ClientProxy", serverSide = "zdoctor.bmw.common.CommonProxy")
 	public static CommonProxy proxy;
 
